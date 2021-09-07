87068fb2db2d38454205478a03ed5253352703c6
sun <1641442162@qq.com>
2021-09-07 08:52:05 +0800

    【修改原因】测试用例提交 【修改人】孙美艳 【修改时间】2021.9.7

--- FastApi/scripts/features/recruitment/positions_mgt/test_TS_SR_position_mgt_06.py
+++ FastApi/scripts/features/recruitment/positions_mgt/test_TS_SR_position_mgt_06.py
@@ -12,79 +12,75 @@
 
 import allure
 import pytest
+from FastApi.scripts.conftest import projectName, postName
+
 from FastApi.aws.project import Project, Personnel
 from FastApi.aws.recruitment import Recruitment
 from FastApi.common.logs_handle import Logger
-from FastApi.common.yaml_handle import read_data_from_file_to_params
 from FastApi.conf import env
-from FastApi.scripts.features.recruitment.conftest import projectName, postName
 
 log = Logger().logger
 pro = Project()
 
 person = Personnel(projectName=projectName, userName=env.USERNAME_PM)
 rec = Recruitment()
+
+
 def setup():
     log.info('-----测试用例预制-----')
 
 
-
+@pytest.mark.usefixtures('init_position')
 @allure.feature('项目招聘')
 @allure.story('岗位管理')
-@allure.title('项目经理查看项目招聘信息')
+@allure.title('项目经理查看项目招聘关闭岗位')
 def test_projectRecuitment():
     res = rec.query_recruitment_by_project(projectName)
-    print(res)
-    assert  res['content']['code'] == 0
-    assert  res['content']['data']['item']['projectRecruit'] is not None
+    assert res['content']['code'] == 0
+    projectRecruit = res['content']['data']['item']['projectRecruit']
+    if projectRecruit:
+        for recruit in projectRecruit:
+            if recruit['postName'] == postName:
+                assert recruit['openFlag'] == '0'
+    else:
+        assert False
 
 
-
-
-
-
-@pytest.mark.usefixtures('position_init_open')
+@pytest.mark.usefixtures('open_init_position')
 @allure.feature('项目招聘')
 @allure.story('岗位管理')
-@allure.title('项目经理查看招聘岗位信息')
-def test_positionInfo():
-    res = rec.query_position_info_by_name(postName,projectName)
-    print(res)
-    assert  res['postName'] == postName
+@allure.title('项目经理查看项目招聘打开岗位')
+def test_projectRecuitment():
+    res = rec.query_recruitment_by_project(projectName)
+    assert res['content']['code'] == 0
+    projectRecruit = res['content']['data']['item']['projectRecruit']
+    if projectRecruit:
+        for recruit in projectRecruit:
+            if recruit['postName'] == postName:
+                assert recruit['openFlag'] == '1'
+    else:
+        assert False
 
 
+@pytest.mark.usefixtures('open_init_position')
+@allure.feature('项目招聘')
+@allure.story('岗位管理')
+@allure.title('项目经理查看已打开招聘岗位详细信息')
+def test_positionInfo_open():
+    res = rec.query_position_info_by_name(postName, projectName)
+    assert res['postName'] == postName
+    assert res['openFlag'] == '1'
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@pytest.mark.usefixtures('init_position')
+@allure.feature('项目招聘')
+@allure.story('岗位管理')
+@allure.title('项目经理查看已关闭招聘岗位详细信息')
+def test_positionInfo_close():
+    res = rec.query_position_info_by_name(postName, projectName)
+    assert res['postName'] == postName
+    assert res['openFlag'] == '0'
 
 
 def teardown_module():
     log.info('-----环境操作-----')
-    res = person.query_recruits()
-    for one in res['content']['data']['list']:
-        post = one['postName']
-        person.delete_recruit(postName=post)
---
+++ FastApi/scripts/features/recruitment/positions_mgt/test_TS_SR_position_mgt_07.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+/*
+@author:sun
+@file:test_TS_SR_position_mgt_07.py
+@time:2021/08/26
+*/
+
+测试删除招聘岗位，能够成功删除
+"""
+
+import allure
+
+from FastApi.aws.project import Project, Personnel
+from FastApi.common.logs_handle import Logger
+from FastApi.common.yaml_handle import read_data_from_file_to_params, get_field_name
+from FastApi.conf import env
+from FastApi.scripts.conftest import projectName, startTime, endTime, postName
+
+log = Logger().logger
+pro = Project()
+
+# 提取测试数据
+params = read_data_from_file_to_params("recruitment_data.yaml", case_name='recruitment_data')
+roleName = get_field_name('recruitment_data.yaml', 'recruitment_data', 'roleType')[0]
+
+person = Personnel(projectName=projectName, userName=env.USERNAME_PM)
+
+
+def setup_module(module):
+    log.info('-----测试用例预制-----')
+    # 添加招募岗位
+    person.create_recruit(postName=postName,
+                          postSum='2',
+                          postType='6',
+                          roleType=roleName,
+                          postJobShare='10',
+                          postDescription='招聘测试人员',
+                          startTime=startTime,
+                          endTime=endTime,
+                          userName=env.USERNAME_PM)
+
+
+@allure.feature('项目招聘')
+@allure.story('岗位管理')
+@allure.title('测试删除招聘岗位，能够成功删除')
+def test_del_recruitment():
+    # 删除招募岗位
+    res = person.delete_recruit(postName=postName, userName=env.USERNAME_PM)
+    assert res['content']['code'] == 0
+
+
+def teardown_module(module):
+    log.info('-----环境操作-----')
