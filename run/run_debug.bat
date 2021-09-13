762638ff97f8d6b1c021424d7de1d9ae0977d535
wangdong <13289859620@163.com>
2021-09-13 10:01:49 +0800

    【修改原因】执行脚本修改 【修改人】王东 【修改时间】2021.9.13

--- run/run_debug.bat
+++ run/run_debug.bat
@@ -5,24 +5,17 @@ rmdir %~dp0\run\allure-results
 
 mkdir %~dp0\run\allure-results
 
-pytest %~dp0\..\FastApi\scripts\preset --html=log\report_preset_%date%.html --json=preset.json --alluredir=allure_report
-
-pytest %~dp0\..\FastApi\scripts\features\project_mgt --html=log\report_project_mgt_%date%.html --json=project_mgt.json --alluredir=allure_report
-
-pytest %~dp0\..\FastApi\scripts\features\recruitment --html=log\report_recruitment_%date%.html --json=recruitment.json --alluredir=allure_report
-
-pytest %~dp0\..\FastApi\scripts\features\system_funtion --html=log\report_system_funtion_%date%.html --json=system_funtion.json --alluredir=allure_report
-
-pytest %~dp0\..\FastApi\scripts\features\project_assess --html=log\report_project_assess_%date%.html --json=project_assess.json --alluredir=allure_report
-
-pytest %~dp0\..\FastApi\scripts\features\homepage --html=log\report_homepage_%date%.html --json=homepage.json --alluredir=allure_report
-
-pytest %~dp0\..\FastApi\scripts\features\message --html=log\report_message_%date%.html --json=message.json --alluredir=allure_report
-
-pytest %~dp0\..\FastApi\scripts\features\report --html=log\report_report_%date%.html --json=report.json --alluredir=allure_report
+pytest -s -q %~dp0\..\FastApi\scripts\preset --html=log\report_preset.html --json=preset.json --alluredir=allure_report
+pytest -s -q %~dp0\..\FastApi\scripts\features\project_mgt --html=log\report_project_mgt.html --json=project_mgt.json --alluredir=allure_report
+pytest -s -q %~dp0\..\FastApi\scripts\features\recruitment --html=log\report_recruitment.html --json=recruitment.json --alluredir=allure_report
+pytest -s -q %~dp0\..\FastApi\scripts\features\system_funtion --html=log\report_system_funtion.html --json=system_funtion.json --alluredir=allure_report
+pytest -s -q %~dp0\..\FastApi\scripts\features\project_assess --html=log\report_project_assess.html --json=project_assess.json --alluredir=allure_report
+pytest -s -q %~dp0\..\FastApi\scripts\features\homepage --html=log\report_homepage.html --json=homepage.json --alluredir=allure_report
+pytest -s -q %~dp0\..\FastApi\scripts\features\message --html=log\report_message.html --json=message.json --alluredir=allure_report
+pytest -s -q %~dp0\..\FastApi\scripts\features\report --html=log\report_report.html --json=report.json --alluredir=allure_report
 
 ::allure serve --port 30000 allure_report
 
-%~dp0\send_mail.py
+%~dp0\..\send_mail.py
 
 Pause
