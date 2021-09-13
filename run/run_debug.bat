5b0565155d4067cdd0ed6a09d04b40dfbe318293
wangdong <13289859620@163.com>
2021-09-13 15:53:40 +0800

    【修改原因】执行脚本修改 【修改人】王东 【修改时间】2021.9.13

--- run/run_debug.bat
+++ run/run_debug.bat
@@ -5,21 +5,21 @@ rmdir /s /q %~dp0\allure-results
 
 mkdir %~dp0\allure-results
 
-::pytest -s -q %~dp0\..\FastApi\scripts\preset --html=%~dp0\log\report_preset.html --json=%~dp0\preset.json --alluredir=%~dp0\allure-results
+::pytest -s -q %~dp0\..\FastApi\scripts\preset --html=%~dp0\log\report_preset.html --self-contained-html --json=%~dp0\preset.json --alluredir=%~dp0\allure-results
 
-::pytest -s -q %~dp0\..\FastApi\scripts\features\project_mgt --html=%~dp0\log\report_project_mgt.html --json=%~dp0\project_mgt.json --alluredir=%~dp0\allure-results
+::pytest -s -q %~dp0\..\FastApi\scripts\features\project_mgt --html=%~dp0\log\report_project_mgt.html --self-contained-html --json=%~dp0\project_mgt.json --alluredir=%~dp0\allure-results
 
-pytest -s -q %~dp0\..\FastApi\scripts\features\recruitment --html=%~dp0\log\report_recruitment.html --json=%~dp0\recruitment.json --alluredir=%~dp0\allure-results
+pytest -s -q %~dp0\..\FastApi\scripts\features\recruitment --html=%~dp0\log\report_recruitment.html --self-contained-html --json=%~dp0\recruitment.json --alluredir=%~dp0\allure-results
 
-::pytest -s -q %~dp0\..\FastApi\scripts\features\system_funtion --html=%~dp0\log\report_system_funtion.html --json=%~dp0\system_funtion.json --alluredir=%~dp0\allure-results
+::pytest -s -q %~dp0\..\FastApi\scripts\features\system_funtion --html=%~dp0\log\report_system_funtion.html --self-contained-html --json=%~dp0\system_funtion.json --alluredir=%~dp0\allure-results
 
-::pytest -s -q %~dp0\..\FastApi\scripts\features\project_assess --html=%~dp0\log\report_project_assess.html --json=%~dp0\project_assess.json --alluredir=%~dp0\allure-results
+::pytest -s -q %~dp0\..\FastApi\scripts\features\project_assess --html=%~dp0\log\report_project_assess.html --self-contained-html --json=%~dp0\project_assess.json --alluredir=%~dp0\allure-results
 
-::pytest -s -q %~dp0\..\FastApi\scripts\features\homepage --html=%~dp0\log\report_homepage.html --json=%~dp0\homepage.json --alluredir=%~dp0\allure-results
+::pytest -s -q %~dp0\..\FastApi\scripts\features\homepage --html=%~dp0\log\report_homepage.html --self-contained-html --json=%~dp0\homepage.json --alluredir=%~dp0\allure-results
 
-::pytest -s -q %~dp0\..\FastApi\scripts\features\message --html=%~dp0\log\report_message.html --json=%~dp0\message.json --alluredir=%~dp0\allure-results
+::pytest -s -q %~dp0\..\FastApi\scripts\features\message --html=%~dp0\log\report_message.html --self-contained-html --json=%~dp0\message.json --alluredir=%~dp0\allure-results
 
-::pytest -s -q %~dp0\..\FastApi\scripts\features\report --html=%~dp0\log\report_report.html --json=%~dp0\report.json --alluredir=%~dp0\allure-results
+::pytest -s -q %~dp0\..\FastApi\scripts\features\report --html=%~dp0\log\report_report.html --self-contained-html --json=%~dp0\report.json --alluredir=%~dp0\allure-results
 
 ::allure serve --port 30000 allure-results
 
